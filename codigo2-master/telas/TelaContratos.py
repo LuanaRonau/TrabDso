c79dd5bf236f505100cb55b5fb292da938f04ce5
Luana Ronau <luana.ronau.m@gmail.com>
2023-05-08 19:25:48 -0300

    diaoito

---
+++ codigo2-master/telas/TelaContratos.py
@@ -0,0 +1,24 @@
+class TelaContratos:
+
+    def __init__(self):
+        pass
+
+    def mostra_opcoes(self):
+        opcao = input("\nTela de gerenciamento dos contratos"
+                      "\n1 - Registrar contrato"
+                      "\n2 - Alterar contrato"
+                      "\n3 - Excluir contrato"
+                      "\n4 - Listar todos os contratos"
+                      "\n5 - Listar contratos por data de realização"
+                      "\n6 - Listar histórico de um funcionário"
+                      "\n7 - Retornar"
+                      "\nDigite a opção do menu que deseja:")
+        
+    def pega_dados_contrato(self):
+        pass
+
+    def mostra_contrato(self):
+        pass
+
+    def seleciona_contrato(self):
+        pass
---
+++ codigo2-master/telas/TelaTransf.py
@@ -0,0 +1,23 @@
+class TelaTransf:
+
+    def __init__(self):
+        pass
+
+    def mostra_opcoes(self):
+        opcao = input("\nTela de gerenciamento de transferências"
+                      "\n1 - Registrar transferência"
+                      "\n2 - Alterar transferência"
+                      "\n3 - Excluir transferência"
+                      "\n4 - Listar todas as transferências"
+                      "\n5 - Listar transferências por data de realização"
+                      "\n6 - Retornar"
+                      "\nDigite a opção do menu que deseja:")
+        
+    def pega_dados_transf(self):
+        pass
+
+    def mostra_transf(self, dados_transf):
+        pass
+
+    def seleciona_transf(self):
+        pass
